87408d99e776112605b2eef3036191cbc871648e
eom175 <eom175@naver.com>
2024-09-14 14:00:22 +0900

    test1

---
+++ .vscode/test1/src/set.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+
+
+public class set {
+    public static void main(String[] args) {
+       HashSet<String> set= new HashSet<>();
+
+     set.add("Jump");
+     set.addAll(Arrays.asList("to", "jaca"));
+
+     System.out.println(set);
+
+
+    }
+        
+    
+    
+}
