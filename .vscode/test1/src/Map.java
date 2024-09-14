2ea65ff12debc523397f875cddb224620c946943
eom175 <eom175@naver.com>
2024-09-14 14:04:21 +0900

    test1

---
+++ .vscode/test1/src/Map.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+public class Map {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("people", "사람");
+        map.put("baseball", "야구");
+        System.out.println(map.get("people"));
+        
+
+
+
+
+    }
+    
+}
